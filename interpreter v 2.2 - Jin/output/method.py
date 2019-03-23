5de82f9ee4678a42590f5b56538e3b50bac21b28
Jinique <44308010+Jinique@users.noreply.github.com>
2019-03-24 05:56:46 +1300

    v2.3

---
+++ interpreter v 2.2 - Jin/output/method.py
@@ -0,0 +1,15 @@
+class Method:
+
+    def __init__(self):
+        self.name: str
+        self.input: str
+        self.return_type: str
+
+    def get_return(self, new_return) ->str:
+        # ToDo
+        pass
+
+    def __str__(self) ->str:
+        # ToDo
+        pass
+
